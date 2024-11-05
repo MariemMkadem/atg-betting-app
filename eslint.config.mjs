0b223a875591fdc6b3137688f96c506096ecdcde
MariemMkadem <mmkadem11@gmail.com>
2024-11-05 17:30:16 +0100

    initialize project with Create React App and TypeScript template

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+// @ts-ignore
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: "error",
+      "prefer-const": "warn",
+    },
+  },
+];
