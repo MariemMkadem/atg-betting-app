cb0909d4b0392949e688f497107fcfa26a4a40dd
MariemMkadem <mmkadem11@gmail.com>
2024-11-06 16:44:23 +0100

    adding .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
-import { BASE_URL } from "../constants";
+
+const baseUrl =
+  process.env.REACT_APP_BASE_URL ||
+  "https://www.atg.se/services/racinginfo/v1/api"; //
 
 export const getBetTypeResults = async (betType: string) => {
-  const response = await axios.get(`${BASE_URL}/products/${betType}`);
+  const response = await axios.get(`${baseUrl}/products/${betType}`);
   return response;
 };
 
 export const getGameDetails = async (id: string) => {
-  const response = await axios.get(`${BASE_URL}/games/${id}`);
+  const response = await axios.get(`${baseUrl}/games/${id}`);
   return response;
 };
